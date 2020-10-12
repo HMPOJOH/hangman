c3101dfa2054d4730c74ccfb0362b679dbdd23c4
HM\POJOH <pontus.johansson@hm.com>
2020-10-12 13:45:24 +0200

    added a prototype for hangman

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Hangman
 {
@@ -10,18 +11,46 @@ namespace Hangman
             //WE LIKE C#
 
             string word = "we like c#";
+            
 
             char[] wordToCharArray = word.ToCharArray();
+            char[] currentStatus = new char[wordToCharArray.Length];
 
-            foreach (var character in wordToCharArray)
+            for (int i = 0; i < wordToCharArray.Length; i++)
+            {
+                if (wordToCharArray[i] == ' ')
+                    currentStatus[i] = ' ';
+                else
+                    currentStatus[i] = '_';
+
+
+            }
+      
+
+
+         
+            while (currentStatus.Contains('_')){
+                //print current status
+
+               // foreach (var character in wordToCharArray)
+                //    Console.Write(character);
+
+                foreach (var character in currentStatus)
                 Console.Write(character);
+         
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Guess a character:");
+                string guess = Console.ReadLine();
+
+                for (int i=0;i< wordToCharArray.Length;i++)
+                    if (wordToCharArray[i] == guess.ToCharArray()[0])
+                        currentStatus[i] = guess.ToCharArray()[0];
 
 
-         //   Console.WriteLine("__ ____ __");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Guess the sentence:");
-            
+
+            }
+
 
         }
     }
