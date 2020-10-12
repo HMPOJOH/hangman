141a569d5f6d8def8c149ccd46e8c283a9e07aec
HM\POJOH <pontus.johansson@hm.com>
2020-10-12 15:23:17 +0200

    added validations for checking if player already guessed the characters or not

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
+using System.Text;
 
 namespace Hangman
 {
@@ -28,39 +30,96 @@ namespace Hangman
                 if (wordToCharArray[i] == ' ')
                     currentStatus[i] = ' ';
                 else
-                    currentStatus[i] = '_';
+                    currentStatus[i] = '-';
 
 
             }
 
 
-            int attempts = 10;
+            int attempts = word.Replace(" ","").Length;
 
-         
-            while (currentStatus.Contains('_') && attempts > 0){
+            StringBuilder guesses = new StringBuilder();
+
+          
+
+            while (currentStatus.Contains('-') && attempts > 0){
+                Console.ForegroundColor = ConsoleColor.White;
                 //print current status
+                foreach (var character in currentStatus)  
+                    Console.Write(character);
+                Console.WriteLine();
 
-                // foreach (var character in wordToCharArray)
-                //    Console.Write(character);
 
-                Console.WriteLine($"attempts left: {attempts}");
-                foreach (var character in currentStatus)
-                Console.Write(character);
+                //print guesses
+                Console.WriteLine(guesses.ToString());
+                    
+
+                            Console.WriteLine($"attempts left: {attempts}");
+                
          
-                Console.WriteLine("");
-                Console.WriteLine("");
+               
                 Console.WriteLine("Guess a character:");
                 string guess = Console.ReadLine();
 
-                for (int i=0;i< wordToCharArray.Length;i++)
-                    if (wordToCharArray[i] == guess.ToCharArray()[0])
-                        currentStatus[i] = guess.ToCharArray()[0];
+                if (guess == "")
+                {
+                    Console.Clear();
+                    continue;
+                    
+                }
+                    
 
-                attempts--;
+
+                //change _ into character if a match
+
+                bool isCorrectGuess = false;
+                for (int i=0;i< wordToCharArray.Length; i++) { 
+                    if (wordToCharArray[i] == guess.ToCharArray()[0]) { 
+                        currentStatus[i] = guess.ToCharArray()[0];
+                        isCorrectGuess = true;
+       
+                    }
+                   
+                }
+
+
+                if (guesses.ToString().Contains(guess.ToCharArray()[0])) { 
+                    Console.WriteLine("You already guessed the character " + guess.Substring(0, 1));
+                    Console.ReadLine(); //Press enter
+
+
+                    Console.Clear();
+                    continue;
+                }
+                else
+                {
+                    guesses.Append(guess.Substring(0, 1) + " ");
+                    attempts--;
+
+                    if (isCorrectGuess)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("CORRECT!");
+
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("WRONG GUESS!");
+                    }
+
+
+
+                }
+                Console.ReadLine(); //Press enter
+                Console.ForegroundColor = ConsoleColor.White;
+
                 Console.Clear();
+
+
             }
 
-            if(currentStatus.Contains('_'))
+            if(currentStatus.Contains('-'))
                 Console.WriteLine("Bad luck! The correct word is "+ word);
             else
                 Console.WriteLine("Good work! You nailed the word," + word);
